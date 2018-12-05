45c07cf00f8ba33a7e6cce091f781771ceb9667c
Paulus Schoutsen <balloob@gmail.com>
2018-12-05 11:22:21 +0100

    Add Google Analytics

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,52 +11,55 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: "User1",
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+    image: "/img/docusaurus.svg",
+    infoLink: "https://www.facebook.com",
+    pinned: true
+  }
 ];
 
 const siteConfig = {
-  title: 'Home Assistant Data Science', // Title for your website.
-  tagline: 'Explore the data of your home',
-  editUrl: 'https://github.com/home-assistant/data.home-assistant/edit/master/docs/',
-  url: 'https://data.home-assistant.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: "Home Assistant Data Science", // Title for your website.
+  tagline: "Explore the data of your home",
+  editUrl:
+    "https://github.com/home-assistant/data.home-assistant/edit/master/docs/",
+  url: "https://data.home-assistant.io", // Your website URL
+  baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
+  gaTrackingId: "UA-57927901-4",
+
   // Used for publishing and more
-  projectName: 'home-assistant',
-  organizationName: 'home-assistant',
+  projectName: "home-assistant",
+  organizationName: "home-assistant",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'data_index', label: 'Data'},
-    {doc: 'quick_start_index', label: 'Quick Start'},
+    { doc: "data_index", label: "Data" },
+    { doc: "quick_start_index", label: "Quick Start" },
     // {doc: 'tutorial_index', label: 'Tutorials'},
-    {blog: true, label: 'Blog'},
+    { blog: true, label: "Blog" }
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/logo-white.svg',
-  footerIcon: 'img/logo-white.svg',
-  favicon: 'img/favicon.png',
+  headerIcon: "img/logo-white.svg",
+  footerIcon: "img/logo-white.svg",
+  favicon: "img/favicon.png",
 
   /* Colors for website */
   colors: {
-    primaryColor: '#03A9F4',
-    secondaryColor: '#4FC3F7',
+    primaryColor: "#03A9F4",
+    secondaryColor: "#4FC3F7"
   },
 
   /* Custom fonts for website */
@@ -74,27 +77,24 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright:
-    'Copyright © ' +
-    new Date().getFullYear() +
-    ' Home Assistant',
+  copyright: "Copyright © " + new Date().getFullYear() + " Home Assistant",
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
   // scripts: ['https://buttons.github.io/buttons.js'],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/default-social.png',
-  twitterImage: 'img/default-social.png',
+  ogImage: "img/default-social.png",
+  twitterImage: "img/default-social.png",
 
   // Show documentation's last contributor's name.
   // enableUpdateBy: true,
@@ -104,11 +104,11 @@ const siteConfig = {
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: 'https://github.com/home-assistant/home-assistant',
-  repoFrontendUrl: 'https://github.com/home-assistant/home-assistant-polymer',
-  repoFrontendWebsite: 'https://github.com/home-assistant/home-assistant.io',
-  repoDevWebsite: 'https://github.com/home-assistant/developers.home-assistant',
-  repoDataWebsite: 'https://github.com/home-assistant/data.home-assistant',
+  repoUrl: "https://github.com/home-assistant/home-assistant",
+  repoFrontendUrl: "https://github.com/home-assistant/home-assistant-polymer",
+  repoFrontendWebsite: "https://github.com/home-assistant/home-assistant.io",
+  repoDevWebsite: "https://github.com/home-assistant/developers.home-assistant",
+  repoDataWebsite: "https://github.com/home-assistant/data.home-assistant"
 };
 
 module.exports = siteConfig;
