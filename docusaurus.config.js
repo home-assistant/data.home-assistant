73ea0b41e943b71b4fbcdc942c22612cb3edcfd2
Tom Brien <TomBrien@users.noreply.github.com>
2022-01-06 14:06:10 +0000

    Use local search instead of Algolia (#177)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,11 +49,6 @@ module.exports = {
       copyright: `Copyright © ${new Date().getFullYear()} Home Assistant, Inc. Built with Docusaurus.`,
     },
     image: "img/default-social.png",
-    algolia: {
-      apiKey: "9fb6680b99f88f635b0744a4078609bf",
-      indexName: "data-home-assistant",
-      algoliaOptions: {}, // Optional, if provided by Algolia
-    },
   },
   presets: [
     [
@@ -68,6 +63,17 @@ module.exports = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        hashed: true,
+        indexDocs: true,
+        indexBlog: true,
+        highlightSearchTermsOnTargetPage: true,
       },
     ],
   ],
